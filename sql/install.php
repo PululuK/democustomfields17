150b3c054c1682cb9f8f77ea0671f2fbf0c9f69a
PululuK <pululuandre@hotmail.com>
2022-03-04 01:03:51 +0100

    Add concrete examples data handler

--- sql/install.php
+++ sql/install.php
@@ -28,10 +28,20 @@ $sql = array();
 $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'democustomfields17` (
     `id_democustomfields17` int(11) NOT NULL AUTO_INCREMENT,
     `id_product` int(11) unsigned NOT NULL,
+    `my_switch_field_example` tinyint(1) unsigned NOT NULL DEFAULT "0",
+    `my_text_field_example` text DEFAULT NULL,
     `date_add` datetime NOT NULL,
     `date_upd` datetime NOT NULL,
     PRIMARY KEY  (`id_democustomfields17`),
     KEY `id_product` (`id_product`)
+) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;';
+
+$sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'democustomfields17_lang` (
+    `id_democustomfields17` int(11) unsigned NOT NULL,
+    `id_shop` INT(11) UNSIGNED NOT NULL DEFAULT "1",
+    `id_lang` int(11) unsigned NOT NULL ,
+    `my_translatable_text_field_example` text DEFAULT NULL,
+    PRIMARY KEY  (`id_democustomfields17`, `id_shop`, `id_lang`)
 ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;';
 
 foreach ($sql as $query) {
--- src/Form/Product/Hooks/HookDisplayAdminProductsExtraFieldsBuilder.php
+++ src/Form/Product/Hooks/HookDisplayAdminProductsExtraFieldsBuilder.php
@@ -5,13 +5,9 @@
     use PrestaShop\Module\Democustomfields17\Form\Product\Hooks\HookFieldsBuilderInterface;
     use Symfony\Component\Form\FormBuilderInterface;
     use Symfony\Component\Form\Extension\Core\Type\TextType;
-    use PrestaShopBundle\Form\Admin\Type\CategoryChoiceTreeType;
-    use PrestaShopBundle\Form\Admin\Type\ChangePasswordType;
-    use PrestaShopBundle\Form\Admin\Type\CountryChoiceType;
     use Symfony\Component\Form\Extension\Core\Type\TextareaType;
     use PrestaShopBundle\Form\Admin\Type\TranslatableType;
     use PrestaShopBundle\Form\Admin\Type\SwitchType;
-    use PrestaShopBundle\Form\Admin\Type\TextWithLengthCounterType;
     use Module;
 
     class HookDisplayAdminProductsExtraFieldsBuilder implements HookFieldsBuilderInterface
@@ -19,33 +15,20 @@
         public function addFields(FormBuilderInterface $adminFormBuilder, Module $module) :FormBuilderInterface
         {
             $adminFormBuilder
-                ->add('my_text_type_field_exemple', TextType::class, [
-                        'label' => $module->l('My simple text type'),
-                        'attr' => array(
-                            'class' => 'my-custom-class',
-                            'data-hex'=> 'true'
-                        )
+                ->add('my_text_field_example', TextType::class, [
+                    'label' => $module->l('My text'),
+                    'attr' => [
+                        'class' => 'my-custom-class',
+                        'data-hex'=> 'true'
+                    ]
                 ])
-                ->add('my_switch_type_field_exemple', SwitchType::class, [
-                    'label' => $module->l('My switch type'),
-                    'choices' => [
-                        $module->l('ON') => true,
-                        $module->l('OFF') => false,
-                    ],
+                ->add('my_switch_field_example', SwitchType::class, [
+                    'label' => $module->l('My switch')
                 ])
-                ->add('my_translatable_type_field_exemple', TranslatableType::class, [
-                    // we'll have text area that is translatable
-                    'label' => $module->l('My translatable type'),
+                ->add('my_translatable_text_field_example', TranslatableType::class, [
+                    'label' => $module->l('My translatable text'),
                     'type' => TextareaType::class,
                     'locales' => $module->getLocales()
-                ])
-                ->add('meta_title', TextWithLengthCounterType::class, [
-                    'label' => $module->l('My text with length counter type'),
-                    'max_length' => 255,
-                ])
-                ->add('category_id', CategoryChoiceTreeType::class, [
-                    'label' => $module->l('My categorytree type'),
-                    'disabled_values' => [4, 5], // Recommantion : Use something look $module->getDisabledCategoriesIds()
                 ]);
 
             return $adminFormBuilder;
